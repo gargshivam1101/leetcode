0ffb8921cfddecfdba29b3805823ec6dbc974549
Shivam Garg <123777566+gargshivam1101@users.noreply.github.com>
2024-05-18 12:13:51 -0400

    Daily LeetCode Question, May 18

---
+++ DistributeCoinsInBinaryTree_979.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int moves;
+
+    private int dfs(TreeNode curr) {
+        if (curr == null) {
+            return 0;
+        }
+        int left = dfs(curr.left);
+        int right = dfs(curr.right);
+        moves += Math.abs(left) + Math.abs(right);
+        return (curr.val - 1) + left + right;
+    }
+
+    public int distributeCoins(TreeNode root) {
+        moves = 0;
+        dfs(root);
+        return moves;
+    }
+}
