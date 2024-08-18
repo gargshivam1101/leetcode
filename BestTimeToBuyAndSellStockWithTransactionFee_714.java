2e60f85c9f5f80d2c78c61ab70b1478156404a65
Shivam Garg <123777566+gargshivam1101@users.noreply.github.com>
2024-08-17 22:43:52 -0400

    Best Time to Buy and Sell Stock with Transaction Fee

---
+++ BestTimeToBuyAndSellStockWithTransactionFee_714.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy == 0) {
+                    // buy 0 means we need to buy
+                    dp[i][0] = Math.max(-prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                } else {
+                    // buy 1 means we need to sell
+                    dp[i][1] = Math.max(prices[i] - fee + dp[i + 1][0], dp[i + 1][1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
