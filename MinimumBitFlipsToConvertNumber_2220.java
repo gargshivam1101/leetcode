5c6483ed84ae6bde05141f68353be415f217c687
Shivam Garg <123777566+gargshivam1101@users.noreply.github.com>
2024-09-03 21:16:15 -0400

    Minimum Bit Flips to Convert Number

---
+++ MinimumBitFlipsToConvertNumber_2220.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int ans = 0;
+
+        while (xor > 0) {
+            if ((xor & 1) == 1) {
+                ans++;
+            }
+            xor >>= 1;
+        }
+        return ans;
+    }
+}
