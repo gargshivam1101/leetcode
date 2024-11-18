3a3c80c702b0f04b783f20f053c83171d857b604
Shivam Garg <123777566+gargshivam1101@users.noreply.github.com>
2024-11-18 00:21:22 -0500

    Count the Number of Fair Pairs

---
+++ CountTheNumberOfFairPairs_2563.java
@@ -0,0 +1,29 @@
+class Solution {
+    long lower_bound(int[] nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        // 0 1 4 4 5 7
+
+        long ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            long low = lower_bound(nums, i + 1, nums.length - 1, lower - nums[i]);
+            long high = lower_bound(nums, i + 1, nums.length - 1, upper - nums[i] + 1);
+
+            ans += high - low;
+        }
+        return ans;
+    }
+}
