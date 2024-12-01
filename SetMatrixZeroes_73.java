4eff6d7fa7fad08eacfdfdc29c21166164b57b02
Shivam Garg <123777566+gargshivam1101@users.noreply.github.com>
2024-12-01 16:36:03 -0500

    Update SetMatrixZeroes_73.java
    Optimized Solution (from 4ms to 0ms)

--- SetMatrixZeroes_73.java
+++ SetMatrixZeroes_73.java
@@ -1,27 +1,25 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
 
-        List<Integer> rows = new ArrayList<>();
-        List<Integer> cols = new ArrayList<>();
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    cols.add(j);
+                    rows[i] = true;
+                    cols[j] = true;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (rows.contains(i) || cols.contains(j)) {
+                if (rows[i] || cols[j]) {
                     matrix[i][j] = 0;
                 }
             }
         }
-
     }
 }
